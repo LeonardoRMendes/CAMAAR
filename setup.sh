23488a803d86a224eab72a183a4f28d50bcc8031
CarlosEduardoSilvaAlmeida <158619558+CarlosEduardoSilvaAlmeida@users.noreply.github.com>
2025-07-14 00:02:18 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ bundle install
 
 # Setup database
 echo "Setting up database..."
-rails db:create
-rails db:migrate
-rails db:seed
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:seed
 
 echo "Setup complete!"
 echo ""
